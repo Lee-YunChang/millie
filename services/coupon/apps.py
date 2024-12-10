56b1b49adb7df9b8e7fac678219da38812f74cdd
Lee-YunChang <dbsckd512@naver.com>
2024-12-10 21:53:59 +0900

    migrate 설정

---
+++ services/coupon/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class CouponConfig(AppConfig):
+    name = 'services.coupon'
--- services/product/models/product.py
+++ services/product/models/product.py
@@ -1,22 +1,12 @@
 from django.db import models
+
+from services.category.models import Category
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
-    discount_rate = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)  # 예: 20.00 = 20% 할인
+    discount_rate = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
     def __str__(self):
         return self.name
-
-    @property
-    def discounted_price(self):
-        # 상품 자체의 할인율 적용된 가격 반환
-        if self.discount_rate > 0:
-            return self.price * (1 - (self.discount_rate / 100))
-        return self.price
-
-    def apply_coupon(self, coupon: Coupon):
-        # 쿠폰 적용된 최종 가격 계산 로직
-        discounted = self.discounted_price
-        # 쿠폰 할인도 % 단위
-        return discounted * (1 - (coupon.discount_rate / 100))
